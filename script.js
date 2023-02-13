6f2b9af0ba77bb0215809eefb9363b0adcf7d7e2
Damianus Clairvoyance <dclairvoyance@ClairMBP.local>
2023-02-13 14:28:47 +0700

    Add isNearby and base for polygon

--- script.js
+++ script.js
@@ -40,20 +40,59 @@ void main() {
 const canvas = document.getElementById('canvas');
 const gl = setupWebGL(canvas);
 
+let vertices = [
+];
+let colors = [
+];
+
+let isDown = false;
+const offset = (3.5/100) * window.innerHeight;
+let currentModel = "";
+
+const setPolygon = () => {
+    currentModel = "polygon";
+}
+
+const isNearby = (e) => {
+    let nearby = false;
+    let x = (2 * (e.clientX - canvas.offsetLeft)) / canvas.clientWidth - 1;
+    let y = 1 - (2 * (e.clientY - offset - canvas.offsetTop)) / canvas.clientHeight;
+    let verticeNearby = vertices.filter(isNearbyV);
+
+    function isNearbyV(vertice) {
+        return ((vertice[0] - 0.05 < x) && (x < vertice[0] + 0.05) 
+        && (vertice[1] - 0.05 < y) && (y < vertice[1] + 0.05));
+    }
+
+    return (verticeNearby.length > 0);
+}
+
 const mouseMoveListener = (e) => {
     // count mouse's coordinates
-    let x = (2 * (e.clientX - canvas.offsetLeft)) / canvas.clientWidth - 1;
-    let y = 1 - (2 * (e.clientY - canvas.offsetTop)) / canvas.clientHeight;
+    if (isDown) {
+        // convert pixel to clip space (-1 to 1)
+        let x = (2 * (e.clientX - canvas.offsetLeft)) / canvas.clientWidth - 1;
+        let y = 1 - (2 * (e.clientY - offset - canvas.offsetTop)) / canvas.clientHeight;
+        vertices[vertices.length-1][0] = x;
+        vertices[vertices.length-1][1] = y;
+        vertices[vertices.length-2][1] = y;
+        vertices[vertices.length-3][0] = x;
+    }
 }
 
 canvas.addEventListener('mousedown', (e) => {
-    console.log("down");
+    // convert pixel to (-1 to 1)
     let x = (2 * (e.clientX - canvas.offsetLeft)) / canvas.clientWidth - 1;
-    let y = 1 - (2 * (e.clientY - canvas.offsetTop)) / canvas.clientHeight;
-    console.log(x, y);
+    let y = 1 - (2 * (e.clientY - offset - canvas.offsetTop)) / canvas.clientHeight;
+    for (let i = 0; i < 4; i++) {
+        vertices.push([x, y]);
+        colors.push([0, 0, 0, 1]);
+    }
+    isDown = true;
 })
+
 canvas.addEventListener("mouseup", (e) => {
-    console.log("up");
+    isDown = false;
 })
 
 gl.viewport(0, 0, canvas.width, canvas.height);
@@ -70,14 +109,6 @@ const cBuffer = gl.createBuffer();
 render();
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT);
-    const vertices = [
-        [-0.7, 0.7], [0.3, 0.7], [-0.7, -0.3], [0.3, -0.3],
-        [-0.3, 0.3], [0.7, 0.3], [-0.3, -0.7], [0.7, -0.7]
-    ];
-    const colors = [
-        [1, 0, 0, 1], [1, 0, 0, 1], [1, 0, 0, 1], [1, 0, 0, 1],
-        [0, 1, 0, 1], [0, 1, 0, 1], [0, 1, 0, 1], [0, 1, 0, 1]
-    ];
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
@@ -93,8 +124,11 @@ function render() {
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
 
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-    gl.drawArrays(gl.TRIANGLE_STRIP, 4, 4);
+    // gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertices.length);
+
+    for (let i = 0; i < vertices.length; i+= 4) {
+        gl.drawArrays(gl.TRIANGLE_STRIP, i, 4);
+    }
 
     window.requestAnimFrame(render);
 }
